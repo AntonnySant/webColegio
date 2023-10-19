5fd54f9b9686a0b33bcc53f16c3e7b66dfe739c2
Antonny <antonny.sd@gmail.com>
2023-10-19 11:49:48 -0500

    Carga proyecto Cole
    Carga proyecto Colegio

---
+++ script.js
@@ -0,0 +1,40 @@
+// script.js
+document.getElementById("openModalBtn").onclick = function() {
+    document.getElementById("loginModal").style.display = "block";
+  }
+  
+  document.getElementById("closeModalBtn").onclick = function() {
+    document.getElementById("loginModal").style.display = "none";
+  }
+  
+  window.onclick = function(event) {
+    if (event.target === document.getElementById("loginModal")) {
+      document.getElementById("loginModal").style.display = "none";
+    }
+  }
+
+  
+const openModalButton = document.getElementById("openModal");
+const closeModalButton = document.getElementById("closeModal");
+const registrationModal = document.getElementById("registrationModal");
+
+openModalButton.addEventListener("click", () => {
+  registrationModal.style.display = "block";
+});
+
+closeModalButton.addEventListener("click", () => {
+  registrationModal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === registrationModal) {
+    registrationModal.style.display = "none";
+  }
+});
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
